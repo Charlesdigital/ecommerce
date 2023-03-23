c8724094dc2efed45adcf3ce8a1f510be36d30c0
Charlese <charles.easton123@gmail.com>
2023-03-22 23:38:10 -0400

    add Max speed option

--- src/components/field.jsx
+++ src/components/field.jsx
@@ -5,17 +5,36 @@ function field() {
     <div>
         <h1>Find an eBike for that fits your criteria</h1>
     <p>range</p>
-<select id="cars">
-  <option value="10">10</option>
-  <option value="20">20</option>
-  <option value="30">30</option>
-  <option value="40">40</option>
-  <option value="50">50+</option>
+<select>
+  <option value="10">10 KM</option>
+  <option value="20">20 KM</option>
+  <option value="30">30 KM</option>
+  <option value="40">40 KM</option>
+  <option value="50">50 KM</option>
+  <option value="60">60+ KM</option>
+
 </select>
-    <p>speed</p>
+    <p>Max speed</p>
+    <select>
+  <option value="20">20 KM</option>
+  <option value="32">32 KM</option>
+  <option value="45">45 KM</option>
+  <option value="46">45+ KM</option>
+
+</select>
     <p>Frame Model</p>
     <p>wheel Size</p>
+    <select>
+  <option value="10">10KM</option>
+  <option value="20">20KM</option>
+  <option value="30">30KM</option>
+  <option value="40">40KM</option>
+  <option value="50">50KM</option>
+  <option value="60">60+ KM</option>
+
+</select>
     <p>price</p>
+
     <p>brand</p>
     </div>
   )
