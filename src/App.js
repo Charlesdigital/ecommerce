843acc40eaa8f659b6d90f36a2e80cf4eaf66dea
Charlese <charles.easton123@gmail.com>
2023-03-22 23:34:29 -0400

    add field option for range

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Field from './components/field'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div> Hello world</div>
+        <Field/>
 
         {/*
         nav
         side panel options
         main div
         footer
-        
+
 
         */}
       </header>
--- src/components/field.jsx
+++ src/components/field.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 
 function field() {
   return (
-    <div>Find an eBike for that fits your criteria
-    </div>
+    <div>
+        <h1>Find an eBike for that fits your criteria</h1>
     <p>range</p>
+<select id="cars">
+  <option value="10">10</option>
+  <option value="20">20</option>
+  <option value="30">30</option>
+  <option value="40">40</option>
+  <option value="50">50+</option>
+</select>
     <p>speed</p>
     <p>Frame Model</p>
     <p>wheel Size</p>
     <p>price</p>
+    <p>brand</p>
+    </div>
   )
 }
 
