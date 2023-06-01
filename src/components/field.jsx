6b20680b649c2a87b5ac2527dc36119843a9fa3b
Charlese <charles.easton123@gmail.com>
2023-05-31 23:06:55 -0400

    add content compenent

--- src/components/field.jsx
+++ src/components/field.jsx
@@ -1,6 +1,38 @@
-import React from "react";
+import {useState} from "react";
+import axios from "axios"
 
 function field() {
+
+  // const [button, getButton] = useState(false)
+
+// const data = () => {
+//   axios.get("https://pokeapi.co/api/v2/pokemon/ditto")
+// }.then((data) => {
+//   console.log(response);
+// })
+// // const data = function() {
+// //   axios.get()
+// // axios()
+// // }
+// }
+
+
+const options = {
+  method: 'GET',
+  url: 'https://aliexpress-datahub.p.rapidapi.com/item_detail',
+  params: {itemId: '3256804591426248'},
+  headers: {
+    'X-RapidAPI-Key': 'fa3caf02d1msh723fb8916116f8ap14b9fajsnba73d390648a',
+    'X-RapidAPI-Host': 'aliexpress-datahub.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
   return (
     <div>
       <h1>Find an eBike for that fits your criteria</h1>
@@ -34,10 +66,14 @@ function field() {
         <option value="29">29 Inch</option>
       </select>
       <p>price</p>
+      <select>
+
       <option value="500">500</option>
         <option value="1000">1000 </option>
         <option value="1500">1500</option>
         <option value="2000">2000</option>
+        </select>
+
       <p>brand</p>
     </div>
   );
